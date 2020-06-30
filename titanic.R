5da3129316b396c380cabd3fd78ae752bbc7485f
khalidaris <khalidalwalid.mohdaris@gmail.com>
2020-06-30 09:09:17 +0800

    add visualization using Pclass column

--- titanic.R
+++ titanic.R
@@ -13,8 +13,10 @@ titanic$Survived <- as.factor(titanic$Survived)
 titanic$Survived <- revalue(titanic$Survived, c("0"="Deceased"))
 titanic$Survived <- revalue(titanic$Survived, c("1"="Alive"))
 
-#create bar graph
-s <- ggplot(data = titanic, aes(x = Survived, fill = Survived))
+#create bar graph to show the number of person that survive or not
+a <- ggplot(data = titanic, aes(x = Survived, fill = Survived))
 plot(s + geom_bar(stat = "count")) + xlab("Passenger Status") + ylab("Frequency")
 
-
+#create bar graph to show the number of person that survive or not by categorize based on passenger class
+b <- ggplot(data = titanic, aes(x = Pclass, fill = Survived))
+plot(b + geom_bar(stat = "count")) + xlab("Passenger Class") + ylab("Frequency")
